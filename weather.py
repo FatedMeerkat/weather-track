6db968fdf2d78f62a4c4c5ec82603dde055c4c2b
FatedMeerkat <58115289+FatedMeerkat@users.noreply.github.com>
2019-11-30 17:32:49 +0200

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,13 +1,12 @@
 import requests
 import json
-from Tkinter import *
-from flask import Flask
+from tkinter import *
 
 root = Tk()
 
-key = "yourkey"
+key = "key"
 url = "http://api.openweathermap.org/data/2.5/weather?"
-city = "yourcity"
+city = "city"
 fullurl = url + "appid=" + key + "&q=" + city
 response = requests.get(fullurl) 
 spr = response.json() 
@@ -18,16 +17,17 @@ if spr["cod"] != "404":
     curpres = spr2["pressure"]
     curhumi = spr2["humidity"]
     spr3 = spr["weather"]
-    cel = "Temperature", curtemp - 273.15, "C°"
-    humi = "Humidity:", curhumi,"%"
-    pressure = "Pressure:", curpress, "hPa"
-    print("Temperature:", cel, "Pressure:", curpres)
+    temperature = "Temperature:", curtemp - 272.15,"C"
+    pressure = "Pressure:", curpres,"hPa"
+    humidity = "Humidity:", curhumi,"%"
+    print("Temperature:", curtemp - 273.15, "Pressure:", curpres)
     #if your temp is Celsius then celsius = curtemp - 273.15
 
 else:
     print("No city found.")
+    
 
-root.title("Weather in", city)
+root.title("Weather in the city")
 
 temp = Label(root, text=temperature)
 temp.pack()
@@ -38,13 +38,4 @@ humi.pack()
 pres = Label(root, text=pressure)
 pres.pack()
 
-app = Flask(__name__)
-
-@app.route('/')
-def web():
-    return render_template("index.html", temp1=cel, humid1=humi, press1=pressure, city1=city);
-
-if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8000, debug=True)
-    
 root.mainloop()
